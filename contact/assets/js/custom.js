5201e1f237cf5cf13901bdaf64d4877e6be63710
mohaimenurrahman <mohaimenur888@gmail.com>
2022-07-14 12:06:19 +0600

    contact add

---
+++ contact/assets/js/custom.js
@@ -0,0 +1,49 @@
+const $dropdown = $(".dropdown");
+const $dropdownToggle = $(".dropdown-toggle");
+const $dropdownMenu = $(".dropdown-menu");
+const showClass = "show";
+
+$(window).on("load resize", function() {
+    if (this.matchMedia("(min-width: 768px)").matches) {
+        $dropdown.hover(
+            function() {
+                const $this = $(this);
+                $this.addClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "true");
+                $this.find($dropdownMenu).addClass(showClass);
+            },
+            function() {
+                const $this = $(this);
+                $this.removeClass(showClass);
+                $this.find($dropdownToggle).attr("aria-expanded", "false");
+                $this.find($dropdownMenu).removeClass(showClass);
+            }
+        );
+    } else {
+        $dropdown.off("mouseenter mouseleave");
+    }
+});
+
+
+$(".board-of-directors").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: true,
+    center: true,
+
+    responsive: {
+        0: {
+            items: 1,
+        },
+        600: {
+            items: 3,
+        },
+        1000: {
+            items: 3,
+        },
+    },
+});
+
+
+
